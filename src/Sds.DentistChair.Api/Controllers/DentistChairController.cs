82b524558c894e0dcaf8777dcf80688e41536d10
inoccard <inocenciocardoso19@gmail.com>
2024-06-16 00:29:06 -0300

    Add project files.

---
+++ src/Sds.DentistChair.Api/Controllers/DentistChairController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sds.DentistChair.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DentistChairController(ILogger<DentistChairController> logger) : ControllerBase
+    {
+        
+    }
+}
